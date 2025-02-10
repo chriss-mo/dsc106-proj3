2cad727a55d0e8b261558ab4af0f365e2d8c6482
csmo1112 <mo.chris1112@gmail.com>
2025-02-10 14:29:01 -0800

    manually classify logged foods

---
+++ test.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import glob, os
+import numpy as np
+import re
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
+import json
+
+entire_dataset = pd.DataFrame()
+for i,file in enumerate(glob.glob('./data/*.csv')):
+    df_temp = pd.read_csv(file)
+    df_temp['subject'] = np.ones(df_temp.shape[0]) * (i+1)
+    entire_dataset = pd.concat([entire_dataset, df_temp])
+
+foods = entire_dataset['logged_food'].unique()
+
+food_classes = {}
+for i in range(foods.shape[0]):
+    food = foods[i]
+    label = input(f'{food}: ')
+    food_classes[food] = label
+
+filename = "food_classes.json"
+
+with open(filename, 'w') as file:
+    json.dump(food_classes, file, indent=4)
